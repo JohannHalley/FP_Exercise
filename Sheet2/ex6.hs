7b9ef0858b6903c436f93d438f1cbf78c7565762
hanbin <hanbin.9797@gmail.com>
2021-05-09 03:06:36 +0200

    6 done

---
+++ Sheet2/ex6.hs
@@ -0,0 +1,15 @@
+posSublistSums :: [Int] -> [Int]
+posSublistSums = subListSums.getPos
+
+subListSums :: [Int] -> [Int]
+subListSums (x:y:xs) = x:subListSums((x + y):xs)
+subListSums x = x
+
+getPos :: [Int] -> [Int]
+getPos = filter (\x -> x >= 0)
+
+{-
+:l ex6
+posSublistSums [1,-2,3,-4,5]
+
+-}
